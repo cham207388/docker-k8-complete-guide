0ae7b9f3f9f405c00b98bf98d8977beb8b31327c
Alhagie Bai Cham <cham207388@gmail.com>
2024-03-19 11:58:28 -0500

    using docker compose to connect node and redis

--- visits/index.js
+++ visits/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const redis = require("redis");
 
+const PORT = 8081
+
 const app = express();
 const client = redis.createClient({
   host: "redis-server",
@@ -10,11 +12,11 @@ client.set('visits', 0)
 
 app.get("/", (req, res) => {
   client.get("visits", (err, visits) => {
-    res.send("Number of visits: is: " + visits);
-    client.send("visits", +visits + 1);
+    res.send("Number of visits: " + visits);
+    client.set("visits", parseInt(visits) + 1);
   });
 });
 
-app.listen(8080, () => {
-  console.log("listening on port 8081");
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
 });
