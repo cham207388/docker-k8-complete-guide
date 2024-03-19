2304c0afd82e46686912a39f57afe5de6da40b65
Alhagie Bai Cham <cham207388@gmail.com>
2024-03-19 07:58:18 -0500

    Fundamentals are covered
    1. how to setup docker file
    2. some error and how to fix like what is available on the base image
    3. caching by separating some operations into steps
    COPY was divided into
    - copy package.json
    COPY ./package.json ./
    - copy the rest
    COPY ./ ./
    This way, if nothing changes in the package.json, and nothing above copying it, then cache will be used

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi there');
+    res.send('Hello world! Docker is the way to build');
 });
 
 app.listen(8080, () => {
-    console.log('listening on port 8080');
+    console.log('listening on port 8080!');
 })
