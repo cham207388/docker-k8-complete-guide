9e8af2815be6aea72e47892f50692d4a07c2f7f8
Alhagie Bai Cham <cham207388@gmail.com>
2024-03-21 07:34:06 -0500

    flagsmith working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import flagsmith from "@/utils/flagsmith";
 
-export default function Home() {
-  return (
-    <main className="main">
-      <div>
-        <h2>Hello World</h2>
-      </div>
-      <Link href="/about">About</Link>
-    </main>
-  );
+export default async function Home() {
+
+	const flags = await flagsmith.getEnvironmentFlags()
+	return (
+		<main className="main">
+			<div>
+				<h2>Hello World</h2>
+			</div>
+			<Link href="/about">About</Link>
+			<br/>
+			{flags.isFeatureEnabled("search")
+				&& <div>
+					<label>Search</label>
+					<input placeholder="Search"/>
+				</div>}
+		</main>
+	);
 }
